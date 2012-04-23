695340814570bff51dcffabbebb97584f24d748a
Martin Shwalbe <mshwalbe@doyousoft.com>
2012-04-23 10:45:53 +0200

    Correct Version Information, Corrected include Path

--- PHPUnit/Runner/Version.php
+++ PHPUnit/Runner/Version.php
@@ -64,7 +64,7 @@ class PHPUnit_Runner_Version
      */
     public static function id()
     {
-        return '@package_version@';
+        return '3.6';
     }
 
     /**
@@ -72,6 +72,6 @@ class PHPUnit_Runner_Version
      */
     public static function getVersionString()
     {
-        return 'PHPUnit @package_version@ by Sebastian Bergmann.';
+        return 'PHPUnit 3.6 by Sebastian Bergmann.';
     }
 }
